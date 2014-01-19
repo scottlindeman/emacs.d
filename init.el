68b1967aab6b4da0bcfd19d02cf1999a909ab9a6
scottlindeman <scott.lindeman@gmail.com>
2014-01-19 17:12:50 -0500

    removed rudel and added themes and octave mode

--- init.el
+++ init.el
@@ -11,9 +11,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-;;Load Rudel
-(load-file "~/.emacs.d/rudel/rudel-loaddefs.el")
-
 ;;General startup preferences
 (delete-selection-mode t)
 (scroll-bar-mode -1)
@@ -27,11 +24,37 @@
 (other-window 1)
 (shell)
 (other-window 1)
-(set-face-attribute 'default nil :height 100) 
+(set-face-attribute 'default nil :height 100)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+
+;;Save all tempfiles in $TMPDIR/emacs$UID/                                                        
+(defconst emacs-tmp-dir (format "%s/%s%s/" "/tmp" "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
+
+;;Themes
+(load-theme 'wombat)
+
+;; -- File associations --
+;; Rails
+(setq auto-mode-alist (cons '("Gemfile" . ruby-mode) auto-mode-alist))
+;; Javascript
+(setq auto-mode-alist (cons '("\\.js$" . js2-mode) auto-mode-alist))
+;; Octave
+(setq auto-mode-alist (cons '("\\.m$" . octave-mode) auto-mode-alist))
 
 ;;Package management
 (require 'package)
 (package-initialize)
+
+;;flymake hooks
+(require 'flymake-gjshint)
+(add-hook 'js-mode-hook 'flymake-gjshint:load)
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
