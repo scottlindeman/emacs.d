dda489ab1ed6b108602dcb31d1602623f330ce4c
scottlindeman <scott.lindeman@gmail.com>
2014-02-22 13:14:51 -0500

    Reworking init file

--- init.el
+++ init.el
@@ -1,34 +1,57 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-variables)
+(custom-set-faces)
 
-;;General startup preferences
-(delete-selection-mode t)
-(scroll-bar-mode -1)
+;; User Settings
+(setq user-full-name "Scott Lindeman")
+(setq user-mail-address "scott.lindeman@gmail.com")
+
+;; Window Settings
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b"))))
+
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+
+;; Editor Settings
+(require 'cl)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+
 (tool-bar-mode -1)
-(blink-cursor-mode t)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 (show-paren-mode t)
-(column-number-mode t)
-(menu-bar-mode 0)
-;;Start with split screen
-(split-window-horizontally)
-(other-window 1)
-(shell)
-(other-window 1)
-(set-face-attribute 'default nil :height 100)
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
+(setq tab-width 2)
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
+(setq js2-basic-offset tab-width) 
+(setq css-indent-offset tab-width)
 
-;;Save all tempfiles in $TMPDIR/emacs$UID/                                                        
+(set-face-attribute 'default nil :height 100)
+
+(setq column-number-mode t
+      inhibit-startup-screen t
+      initial-scratch-message nil
+      delete-selection-mode t
+      transient-mark-mode t
+      blink-cursor-mode t
+      x-select-enable-clipboard t
+      echo-keystrokes 0.1
+      use-dialog-box nil
+      initial-frame-alist (quote ((fullscreen . maximized)))
+      visible-bell t)
+
+;; Themes
+(load-theme 'wombat)
+
+;; Save all tempfiles in $TMPDIR/emacs$UID/                                                    
 (defconst emacs-tmp-dir (format "%s/%s%s/" "/tmp" "emacs" (user-uid)))
 (setq backup-directory-alist
       `((".*" . ,emacs-tmp-dir)))
@@ -37,31 +60,142 @@
 (setq auto-save-list-file-prefix
       emacs-tmp-dir)
 
-;;Themes
-(load-theme 'wombat)
-
-;; -- File associations --
-;; Rails
-(setq auto-mode-alist (cons '("Gemfile" . ruby-mode) auto-mode-alist))
-;; Javascript
-(setq auto-mode-alist (cons '("\\.js$" . js2-mode) auto-mode-alist))
-;; Octave
-(setq auto-mode-alist (cons '("\\.m$" . octave-mode) auto-mode-alist))
-
-;;Load nxhtml
-(load "~/.emacs.d/nxhtml/autostart.el")
-
-;;Package management
-(require 'package)
+;; Package Management
+(load "package")
 (package-initialize)
-
-;;flymake hooks
-(require 'flymake-gjshint)
-(add-hook 'js-mode-hook 'flymake-gjshint:load)
-
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives 
-	     '("tromey" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives 
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("tromey" . "http://tromey.com/elpa/"))
+
+(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+(defvar scott/packages '(ac-slime
+                         ac-js2
+                         auto-complete
+                         autopair
+                         clojure-mode
+                         clojure-test-mode
+                         coffee-mode
+                         deft
+                         flymake
+                         flymake-css
+                         flymake-python-pyflakes
+                         gist
+                         go-mode
+                         haml-mode
+                         haskell-mode
+                         htmlize
+                         js2-mode
+                         magit
+                         markdown-mode
+                         marmalade
+                         nrepl
+                         o-blog
+                         octave-mod
+                         org
+                         paredit
+                         puppet-mode
+                         restclient
+                         rvm
+                         scala-mode2
+                         sml-mode
+                         yaml-mode)
+  "Default packages")
+
+(defun scott/packages-installed-p ()
+  (loop for pkg in scott/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (scott/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg scott/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;;------------------------------------------------------------------------------
+
+;; Utilities
+;;TODO: Set up ORG
+; Ido
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+
+; Autopair
+(require 'autopair)
+
+; Auto Complete
+(require 'auto-complete-config)
+(ac-config-default)
+
+; Flyspell
+(setq flyspell-issue-welcome-flag nil)
+(setq-default ispell-program-name "/usr/local/bin/aspell")
+(setq-default ispell-list-command "list")
+
+;; Cleanup
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+(defun cleanup-region (beg end)
+  "Remove tmux artifacts from region."
+  (interactive "r")
+  (dolist (re '("\\\\│\·*\n" "\W*│\·*"))
+    (replace-regexp re "" nil beg end)))
+
+(global-set-key (kbd "C-x M-t") 'cleanup-region)
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+;;------------------------------------------------------------------------------
+
+;; Language Specific
+
+; Ruby
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (autopair-mode)))
+
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+
+; Javascript
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-hook 'js2-mode-hook
+	  (lambda ()
+            (autopair-mode)
+            (ac-js2-mode)))
+
+; CSS
+(require 'flymake-css)
+(add-hook 'css-mode-hook
+          (lambda ()
+            (autopair-mode)
+            (flymake-css-load)))
+
+; Python
+(add-hook 'python-mode-hook
+          (lambda ()
+            (autopair-mode)
+            (flymake-python-pyflakes-load)))
